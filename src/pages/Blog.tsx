344714c26d632627efe289b0f1ab559423c32a55
Caio12022 <109630951+Caio12022@users.noreply.github.com>
2026-06-26 10:21:00 -0300

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ArrowRight, Clock, ShieldCheck, Search } from "lucide-react";
 import { artigos } from "../data/artigos";
-
 export default function Blog() {
   const [busca, setBusca] = useState("");
 
