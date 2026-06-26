3b6219b4d454ac72e1d2bc9871eccae443a39af0
Caio12022 <109630951+Caio12022@users.noreply.github.com>
2026-06-26 10:29:49 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
+
+// Wrapper que força o BlogPost a recriar quando o slug muda
+function BlogPostWrapper() {
+  const { slug } = useParams();
+  return <BlogPost key={slug} />;
+}
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blog" element={<Blog />} />
-      <Route path="/blog/:slug" element={<BlogPost />} />
+      <Route path="/blog/:slug" element={<BlogPostWrapper />} />
     </Routes>
   );
 }
