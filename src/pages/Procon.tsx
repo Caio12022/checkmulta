0ded714e0e4e3f348e4d39be74519c7687adec8f
Caio12022 <109630951+Caio12022@users.noreply.github.com>
2026-07-23 21:43:59 -0300

    Update Procon.tsx

--- src/pages/Procon.tsx
+++ src/pages/Procon.tsx
@@ -521,7 +521,7 @@ export default function Procon() {
               alt="CheckMulta"
               width="240"
               height="64"
-              className="h-auto w-full origin-left scale-[1.25] object-contain md:scale-[1.35]"
+              className="h-9 w-auto object-contain md:h-11"
             />
           </a>
 
